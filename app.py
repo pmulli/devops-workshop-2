1d59759e845574ef4aa20358d7fb843446c0cbd6
Paul Mullineux <paul.d.mullineux@aexp.com>
2021-05-06 15:51:01 +0100

    get bus stop codes

--- app.py
+++ app.py
@@ -9,7 +9,24 @@ app_id = "00d644e1"
 
 def getAtcoCodes(postcode):
     assert postcode == 'NW51TL'
-    return ['490008660N','490008660S']
+    url = 'http://api.postcodes.io/postcodes/' + postcode
+    print(url)
+    resp = get(url).json()
+    longitude = resp.get('result').get('longitude')
+    latitude = resp.get('result').get('latitude')
+    print(longitude)
+    print(latitude)
+ 
+    url = 'http://transportapi.com/v3/uk/places.json'
+    codes_response = get(url, params={ 'app_id':app_id,  'app_key' : key, 'lat' : latitude, 'lon' : longitude, 'type' : 'bus_stop'})
+    print(codes_response.json())
+    bus_stop_list = codes_response.json().get('member')
+    bus_stop1 = bus_stop_list[0].get('atcocode')
+    bus_stop2 = bus_stop_list[1].get('atcocode')
+    print(bus_stop1)
+    print(bus_stop2)
+    
+    return [bus_stop1,bus_stop2]
 
 @app.route('/')
 def display_bus_info():
