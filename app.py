8e7e72a0f78e9cd51fa8f2a846073767d5977848
Srikrishna Datla <srikrishna.datla@aexp.com>
2021-05-06 15:40:55 +0100

    Pushing mvp 2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from flask import Flask, request, render_template
+import datetime
 from requests import get
 
 app = Flask(__name__)
@@ -11,12 +12,31 @@ def getAtcoCodes(postcode):
     assert postcode == 'NW51TL'
     return ['490008660N','490008660S']
 
+def parseResponse(busResponse):
+    upcomingBuses = []
+    for lineBuses in busResponse['departures'].values():
+        for upcomingBus in lineBuses:
+            upcomingBuses += [
+                {
+                    "busStop": busResponse['name'],
+                    "lineNumber": upcomingBus['line'],
+                    "direction": upcomingBus['direction'],
+                    "expectedDepartureTime": upcomingBus['best_departure_estimate']
+                }
+            ]
+    return upcomingBuses
+
+def listSort(e):
+    return e['expectedDepartureTime']
+
+
 @app.route('/')
 def display_bus_info():
     resp = []
     for code in getAtcoCodes('NW51TL'):
         url = 'http://transportapi.com/v3/uk/bus/stop/' + code + '/live.json'
         print(url)
-        bus_response = get(url, params={ 'app_id':app_id,  'app_key' : key})
-        resp += [bus_response.json()]
-    return str(resp)
+        busResponse = get(url, params={ 'app_id':app_id,  'app_key' : key})
+        resp += parseResponse(busResponse.json())
+    resp.sort(key=listSort)
+    return render_template('index.html', upcomingBuses=resp, timeStamp=datetime.datetime.now())
